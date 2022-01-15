abd10728cd4e0b6369d9643c965f77a24deb6a73
haraGADygyl <ti60gtt@gmail.com>
2022-01-15 23:13:09 +0200

    lecture 1

---
+++ 01-databases-introduction/01-create-tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `employees` (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+CREATE TABLE `categories` (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `products` (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    category_id INT
+);
---
+++ 01-databases-introduction/02-insert-data-in-tables.sql
@@ -0,0 +1,3 @@
+INSERT INTO employees (first_name, last_name) VALUES ('Ivan', 'Ivanov');
+INSERT INTO employees (first_name, last_name) VALUES ('Jorko', 'Jorkov');
+INSERT INTO employees (first_name, last_name) VALUES ('Pesho', 'Peshev');
