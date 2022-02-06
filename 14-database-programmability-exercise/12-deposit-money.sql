05fa4d3bee0574826dd1d05b558324c22c612642
haraGADygyl <ti60gtt@gmail.com>
2022-02-06 13:08:59 +0200

    add lecture 14

---
+++ 14-database-programmability-exercise/12-deposit-money.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE PROCEDURE usp_deposit_money(account_id INT, money_amount DECIMAL(19, 4))
+BEGIN
+    START TRANSACTION;
+    IF(money_amount <= 0) THEN ROLLBACK;
+    ELSE
+    UPDATE `accounts` AS a
+    SET balance = balance + money_amount
+    WHERE a.`id` = account_id;
+    END IF;
+END $$
+
+CALL usp_deposit_money(1, 10);
