ea0ecb62d5a8fba456fc112dbff0d5b0e2b8d41f
haraGADygyl <ti60gtt@gmail.com>
2022-02-07 23:50:18 +0200

    add exam 20-june-2021

---
+++ 17-exam-20-june-2021/02-insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO clients(`full_name`, `phone_number`)
+(SELECT 
+    CONCAT(d.`first_name`, ' ', d.`last_name`) AS `full_name`, 
+    CONCAT('(088) 9999', d.`id` * 2) as `phone_number`
+FROM drivers AS d
+WHERE d.`id` BETWEEN 10 AND 20);
